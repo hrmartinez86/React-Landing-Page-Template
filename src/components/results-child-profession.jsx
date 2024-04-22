d827ac98c01c009fde2658966eaafcf5a2f20699
hrmartinez86ulfix <ruben@ulfix.com>
2024-04-22 17:44:16 -0600

    add worker card

--- src/components/results-child-profession.jsx
+++ src/components/results-child-profession.jsx
@@ -1,21 +1,29 @@
 export const ResultsChildProfession = ({ data }) => {
   return (
-    <div>
+    <div className="card-container">
       {data ? (
         data.map((d, i) => (
-          <div
-            key={`${d.name}-${i}`}
-            className="col-md-3 col-sm-6 team"
-            id="Results"
-          >
-            <div className="thumbnail">
-              {" "}
-              <div className="caption">
-                <h4>{d.name}</h4>
-                <p>{d.phone}</p>
-              </div>
+          <div className="card">
+            <img src={d.image} alt={d.name} />
+            <div className="card-body">
+              <h5 className="card-title">{d.name}</h5>
+              <p className="card-text">Teléfono: {d.phone}</p>
+              <p className="card-text">Profesión: {d.skills}</p>
             </div>
           </div>
+          // <div
+          //   key={`${d.name}-${i}`}
+          //   className="col-md-3 col-sm-6 team"
+          //   id="Results"
+          // >
+          //   <div className="thumbnail">
+          //     {" "}
+          //     <div className="caption">
+          //       <h4>{d.name}</h4>
+          //       <p>{d.phone}</p>
+          //     </div>
+          //   </div>
+          // </div>
         ))
       ) : (
         <div>
